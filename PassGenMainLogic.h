b8670e53d8d1875c4da625faf633d5428e6e1733
Pouria Dana <pouriadana73@gmail.com>
2025-01-24 23:09:33 +0330

    Add function & class declarations

--- PassGenMainLogic.h
+++ PassGenMainLogic.h
@@ -1,4 +1,38 @@
 #ifndef PASSGENMAINLOGIC_H
 #define PASSGENMAINLOGIC_H
 
+#include <iostream>
+#include <string>
+#include <random>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+#include <string.h>
+
+/* Symbolic constants */
+#define MIN_PASS_LEN 12
+#define MAX_PASS_LEN 100
+
+/* Global variables */
+bool filled[MAX_PASS_LEN];
+
+/* Function declarations */
+bool isdigit(std::string c);
+bool isalphabetic(std::string c);
+void show_help();
+void fill_arr_zero(bool arr[], int len);
+void fill_bytwo(bool arr[], int pos);
+
+/* Class declaration */
+class Rand_int {
+private:
+    std::random_device rd;
+    std::default_random_engine engine;
+    std::uniform_int_distribution<int> distrib;
+public:
+    // class constructor
+    Rand_int(int low, int high);
+    int operator()();
+};
+
 #endif // PASSGENMAINLOGIC_H
