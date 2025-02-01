c54631d81f6550131277875a0a032d7ef8aca48d
Pouria Dana <pouriadana73@gmail.com>
2025-02-01 12:58:33 +0330

    clean up

--- mainwindow.h
+++ mainwindow.h
@@ -28,6 +28,5 @@ private slots:
     void on_generateButton_clicked();
 
     void on_copyButton_clicked();
-    void on_colorInput_cursorPositionChanged(int arg1, int arg2);
 };
 #endif // MAINWINDOW_H
